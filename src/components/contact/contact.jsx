bc3eaacca64be6887eb7bc68b571bdac43c67ff0
kamran-2 <ahmad3k325@gmail.com>
2024-08-18 10:16:42 +0530

    edited

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -5,21 +5,21 @@ import gitlogo from '../../assets/gitlogo.png'
 import linkedinlogo from '../../assets/linkedinlogo.png'
 import './contact.css'
 
-function Contact(){
+function Contact() {
     const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-    emailjs.sendForm('service_69pif56', 'template_q3m8k5f', form.current, 'oQdu9sD-xqvj9LYrg')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+        emailjs.sendForm('service_69pif56', 'template_q3m8k5f', form.current, 'oQdu9sD-xqvj9LYrg')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
 
-   
+
     return (
         <section id="contactPage">
             <div className="contact">
@@ -27,26 +27,25 @@ function Contact(){
                     Contact Me
                 </h2>
                 <div className="contactDesc">
-                 sit amet consectetur adipisicing elit.Nemo obcaecati expedita quod fugiat culpa cumque prov
+                    sit amet consectetur adipisicing elit.Nemo obcaecati expedita quod fugiat culpa cumque prov
                 </div>
                 <form ref={form} action="" className="contactForm" onSubmit={sendEmail}>
-                    <input type="text" name="your_name" className="name" placeholder='your Name'/>
+                    <input type="text" name="your_name" className="name" placeholder='your Name' />
                     <input type="email" name="your_email" className="email" placeholder='Your email' />
-                    <textarea name="message"  className="msg"  placeholder="Your Message" rows="5"></textarea>
+                    <textarea name="message" className="msg" placeholder="Your Message" rows="5"></textarea>
                     <button className="submit" value="send" type='submit'>Submit</button>
                     <div className="links">
-                        <a href=''><img src={instalogo} alt="" className="link" /></a>
+                        <a href='https://www.instagram.com/_ahmad_kamran/'><img src={instalogo} alt="" className="link" /></a>
                         <a href='https://www.linkedin.com/in/ahmadkamran2/'><img src={linkedinlogo} alt="" className="link" /></a>
-                        <a><img src={gitlogo} alt="" className="link" /></a>
-                      
+                        <a href='https://github.com/kamran-2'><img src={gitlogo} alt="" className="link" /></a>
                     </div>
                 </form>
 
-                
+
             </div>
         </section>
     );
-  
+
 }
 
 export default Contact
